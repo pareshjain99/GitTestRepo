32b9e4e409f9489134e2a4fbdc7eb90260e6d4f8
Synechron2 <synechron2@synechrons-macbook-pro-4.local>
2018-09-02 12:10:19 +0400

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Synechron2 on 9/2/18.
+//  Copyright © 2018 Synechron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
